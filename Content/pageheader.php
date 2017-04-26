886c53b509cc8f4a247b82956dcdbb45e222d67b
bakagamedev <noel.abercrombie@hotmail.com>
2017-04-26 21:06:34 +0100

    Navmenu!

--- Content/pageheader.php
+++ Content/pageheader.php
@@ -1,17 +1,27 @@
 <div class="header">
 <a href="index.php">
-<img src="./images/header.jpg" style="max-width:100%"/>
+  <img src="./images/header.jpg" style="max-width:100%;"/>
 </a>
-<div class="headerContent">
-	<a href="index.php">Home</a>
-	<a href="about.php">About</a>
-	Brave Witches -
-	<a href="bwop.php">OP</a>
-	<a href="bw01.php">#01</a>
-	<a href="bw02.php">#02</a>
-	<a href="bw03.php">#03</a>
-	<a href="bw04.php">#04</a>
-	Extra -
-	<a href="swrh.php">Romagnan Holiday</a>
-</div>
-</div>
+
+<nav id="primary_nav_wrap">
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="about.php">About</a></li>
+  <li><a href="#">Episodes</a>
+    <ul>
+      <li><a href="bwop.php">Brave Witches OP</a></li>
+      <li><a href="bw01.php">#01 - The Witch of Sasebo</a></li>
+      <li><a href="bw02.php">#02 - Take Flight Chidori!</a></li>
+      <li><a href="bw03.php">#03 - The 502nd Joint Fighter Wing</a></li>
+      <li><a href="bw04.php">#04 - If you want to fight, Get stronger!</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Extra</a>
+    <ul>
+      <li><a href="swrh.php">Romagnan Holiday</a></li>
+    </ul>
+  </li>
+</ul>
+</nav>
+
+</div>	<!-- Header -->
